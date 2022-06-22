ddeb8ca78c70365491971c4796e21afc41e15620
Дмитрий <gruneiter@gmail.com>
2022-06-23 00:49:30 +0300

    б8

--- src/view/comments-view.js
+++ src/view/comments-view.js
@@ -2,14 +2,14 @@ import AbstractStatefulView from '../framework/view/abstract-stateful-view';
 
 const createTemplate = (data) => {
   const {commentIds} = data;
-  let commentsCurrent = [];
+  let currentComments = [];
   commentIds.forEach((id) => {
-    commentsCurrent.push(data.comments.find((item) => item.id === id));
+    currentComments.push(data.comments.find((item) => item.id === id));
   });
-  commentsCurrent = commentsCurrent.filter((comment) => comment !== undefined);
+  currentComments = currentComments.filter((comment) => comment !== undefined);
   return `
     <section class="film-details__comments-wrap">
-    <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${ commentsCurrent.length }</span></h3>
+    <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${ currentComments.length }</span></h3>
 
     <ul class="film-details__comments-list"></ul>
   </section>
